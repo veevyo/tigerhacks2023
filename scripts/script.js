5808beeaf7d76f6015aaf1d5089a5549720a8fa2
vivian <41368076+veevyo@users.noreply.github.com>
2023-11-04 12:16:45 -0500

    YAYAYYAYAYA

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,22 @@
-import { TmdbClient }  from '../lib/tmdb-js-1.0.3/src/tmdb-js/tmdb-js'
+const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZGE3OTI3M2MxZTMzNTcyMzE1N2JmZTQ0YTEzZjNhMCIsInN1YiI6IjY1NDU3NTMwMWFjMjkyMDBhYmQ4NDFhZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.WmAGa5131zPQHt-0okCVjDq66nwhuUuSySijS0M-GUU'
+    }
+};
+function test() {
+    fetch('https://api.themoviedb.org/3/search/keyword?query=andrew', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+}
+  
+
+//import { TmdbClient }  from '../lib/tmdb-js-1.0.3/src/tmdb-js/tmdb-js'
 
 
-searchName = async function() {
+/*searchName = async function() {
 
     let name = document.getElementById("name").value;
 
@@ -17,4 +32,4 @@ searchName = async function() {
     let searchSection = tmdb.getSearchSection();
     let searchResult = await searchSection.multiSearchAsync(name);
     console.log(searchResult);
-}
+}*/
