a93f876822319c5a935c83c267c6e3e4c6eaaa53
vivian <41368076+veevyo@users.noreply.github.com>
2023-11-04 15:41:56 -0500

    enter submits

--- scripts/script.js
+++ scripts/script.js
@@ -98,25 +98,3 @@ async function test() {
 
     
 }
-  
-
-//import { TmdbClient }  from '../lib/tmdb-js-1.0.3/src/tmdb-js/tmdb-js'
-
-
-/*searchName = async function() {
-
-    let name = document.getElementById("name").value;
-
-    let auth = getAuthenticator("87594bb664c20412e3f05addd6da739b")
-
-    let apiKey = auth.apiKey;
-    //let username = auth.username;
-    //let password = auth.password;
-
-    let tmdb = new TmdbClient(apiKey);
-
-    //search
-    let searchSection = tmdb.getSearchSection();
-    let searchResult = await searchSection.multiSearchAsync(name);
-    console.log(searchResult);
-}*/
